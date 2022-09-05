23eb261be264ee1c000bb138c12cc249caf8a799
FabrizioCasanova <fabri.casanova@outlook.com>
2022-09-05 13:19:13 -0300

    Corrigiendo cosas del carrito

--- script-js/script.js
+++ script-js/script.js
@@ -1,26 +1,3 @@
-/*class Producto {
-  constructor(nombre, talle, stock, precio, imagen) {
-    this.nombre = nombre
-    this.talle = talle
-    this.stock = stock
-    this.precio = precio
-    this.imagen = imagen
-  }
-}
-
-const producto1 = new Producto("Conjunto Sexy Lali", 90, "Quedan 30", 2100, "./img/sexyLali.webp")
-const producto2 = new Producto("Conjunto Xy ART.1303", "M", "Quedan 24", 1050, "./img/xy-1303.webp")
-const producto3 = new Producto("Pack de bombacha ART.30211", 95, "Quedan 20", 890, "./img/packBombacha1.webp")
-const producto4 = new Producto("Pack de bombacha ART.30213", 85, "Quedan 26", 800, "./img/packBombacha2.webp")
-const producto5 = new Producto("Conjunto de Andressa ART.922", 80, "Quedan 15", 1450, "./img/conjuntoAndressa-922.webp")
-const producto6 = new Producto("Boxer Dufour ART.11812", "L", "Quedan 12", 850, "./img/conjuntoAndressa-11812.webp")
-const producto7 = new Producto("Conjunto de Andressa ART.669", 100, "Quedan 10", 1150, "./img/conjuntoAndressa-669.webp")
-const producto8 = new Producto("Conjunto de Andressa ART.1080", 105, "Quedan 22", 1400, "./img/conjuntoAndressa-1080.webp")
-
-const productosArray = [producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8]
-*/
-
-
 // Funcion para optimizar el pintado de Dom en 3 divs distintos //
 
 function cardsDom(divHtml, product) {
@@ -39,10 +16,25 @@ function cardsDom(divHtml, product) {
   `
 }
 
+// Funcion asincronica
+
+async function consultaAsync() {
+  const datosFetch = await fetch('./json/productos.json')
+  const productosJson = await datosFetch.json()
+  return productosJson
+}
+
+function aplicarNumeroCarrito(cantidad) {
+  const numCarrito = document.getElementById('cantidadCarrito')
+  numCarrito.innerHTML = `
+  <span class= "badge bg-primary rounded-pill"> ${cantidad} </span>
+  `
+}
+
 const productosHtml = document.getElementById("productosHtml")
 const productosHtml2 = document.getElementById("productosHtml2")
 const productosHtml3 = document.getElementById("productosHtml3")
-const carritoProductos = JSON.parse(localStorage.getItem('carrito')) ?? []
+const storageCarrito = JSON.parse(localStorage.getItem('carrito')) ?? []
 
 fetch('./json/productos.json')
   .then(response => response.json())
@@ -63,41 +55,129 @@ fetch('./json/productos.json')
 
     });
 
+    class Producto {
+      constructor(nombre, talle, stock, precio, imagen) {
+        this.nombre = nombre
+        this.talle = talle
+        this.stock = stock
+        this.precio = precio
+        this.imagen = imagen
+      }
+    }
+
+    let ultimasComprasStorage = []
     productosArray.forEach((producto) => {
 
       const cardProd = document.getElementById(`${producto.id}`)
       cardProd.children[1].children[4].addEventListener('click', () => {
+
+        const pintarCarrito = document.getElementById("pintarCarrito")
+
+        const product = new Producto(producto.nombre, producto.talle, producto.stock, producto.precio, producto.imagen)
+        ultimasComprasStorage.push(product)
+        aplicarNumeroCarrito(ultimasComprasStorage.length)
         
-        Swal.fire(
-          '¡Listo!',
-          '¡Tu producto fue agregado al carrtio con exito! ',
-          'success',
-        )
+        // 1. Pintada del Dom de mi carrito sin actualizar la pagina
+        pintarCarrito.innerHTML += ` 
+          <div class="card border-primary mb-3" style="max-width: 20rem;">
+          <div class="card-header"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">${product.nombre}</font></font></div>
+          <div class="card-body">
+            <p class="card-text"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"> Cantidad: ${product.cant}</font></font></p>
+            <p class="card-text"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"> Talle: ${product.talle}</font></font></p>
+            <p class="card-text"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"> Stock: ${product.stock}</font></font></p>
+            <p class="card-text"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"> Precio: $${product.precio}</font></font></p>
+          </div>
+        </div>  
+          `
 
-        if(carritoProductos.find(product => product.id == producto.id)) {
-          let indiceCarrito = carritoProductos.findIndex((product => product.id == producto.id))
-          if(carritoProductos[indiceCarrito].cantidad < producto.stock) {
-            carritoProductos[indiceCarrito].cantidad++
+        if (storageCarrito.find(product => product.id == producto.id)) {
+          let indiceCarrito = storageCarrito.findIndex((product => product.id == producto.id))
+          if (storageCarrito[indiceCarrito].cantidad < producto.stock) {
+            storageCarrito[indiceCarrito].cantidad++ // 1. replicar esto en la pintada del Dom de mi carrito para declarar la cantidad
+            localStorage.setItem('carrito', JSON.stringify(storageCarrito))
+
+            Swal.fire(
+              '¡Listo!',
+              '¡Tu producto fue agregado al carrtio con exito! ',
+              'success',
+            )
+
+
+          } else if ((storageCarrito[indiceCarrito].cantidad = producto.stock)) {
+            Swal.fire(
+              'Error!',
+              '¡Ya no hay stock de este producto! ',
+              'error',
+            )
           }
-        } else{
-          const crearProducto = {id: producto.id, cantidad: 1}
-          carritoProductos.push(crearProducto) 
+        } else {
+          const crearProducto = {
+            id: producto.id,
+            cantidad: 1,
+            nombre: producto.nombre,
+            talle: producto.talle,
+            img: producto.imagen,
+            precio: producto.precio,
+            stock: producto.stock
+          } 
+
+          storageCarrito.push(crearProducto)
+          localStorage.setItem('carrito', JSON.stringify(storageCarrito))
+          Swal.fire(
+            '¡Listo!',
+            '¡Tu producto fue agregado al carrtio con exito! ',
+            'success',
+          )
         }
-
-        localStorage.setItem('carrito', JSON.stringify (carritoProductos))
-
       })
     })
   })
 
-  let prueba = []
-  let ultimasComprasStorage = []
+  // Pintada de Dom para para mi carrito al actualizarse la pagina
 
-  if(localStorage.getItem('carrito')) {
-    ultimasComprasStorage = JSON.parse(localStorage.getItem('carrito'))
-    ultimasComprasStorage.forEach((compra) =>{
-      prueba.push(compra)
-      console.log(`${prueba.nombre}`)
-        
-    })
-  }
+const pintarCarrito = document.getElementById("pintarCarrito")
+
+if (localStorage.getItem('carrito')) { 
+
+  ultimasComprasStorage = JSON.parse(localStorage.getItem('carrito'))
+
+  aplicarNumeroCarrito(ultimasComprasStorage.length)
+
+  ultimasComprasStorage.forEach((compra) => {
+
+    pintarCarrito.innerHTML += `
+        <div class="card border-primary mb-3" " style="max-width: 20rem;">
+          <div class="card-header"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">${compra.nombre}</font></font></div>
+          <div class="card-body">
+            <p class="card-text"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"> Cantidad: ${compra.cantidad}</font></font></p>
+            <p class="card-text"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"> Talle: ${compra.talle}</font></font></p>
+            <p class="card-text"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"> Stock: ${compra.stock}</font></font></p>
+            <p class="card-text"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"> Precio: $${compra.precio}</font></font></p>
+          </div>
+        </div>  
+          `
+  })
+}
+
+const numCarrito = document.getElementById('cantidadCarrito')
+
+function limpiarHTML() {
+  pintarCarrito.innerHTML = ''
+  numCarrito.innerHTML = ''
+}
+
+const limpiarCarrito = document.getElementById('limpiarCarrito')
+
+limpiarCarrito.addEventListener('click', () => {
+  ultimasComprasStorage = [] 
+  limpiarHTML()
+})
+
+
+// 1. Declarar en mi array de compras (no de mi localStorage) la variable cantidad 
+
+// 2. Al borrar el contenido de mi carrito que se borre el LocalStorage
+
+// 3. Que no se creen nuevas cards en el dom con mi array de compras (no de mi localStorage)
+
+// 4. actualizar en mi array de compras (no de mi localStorage) el numerito que indica cuantos productos tengo en mi carrito
